c2e971670f16d54add37cae1bba7a8d2ccbf9333
Kuan-Yu Chang <78361822+as38063997@users.noreply.github.com>
2025-03-03 22:14:29 -0500

    new changes in camera and sonar

--- HCSR04.py
+++ HCSR04.py
@@ -1,83 +1,126 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
+import threading
+
+# (Optional) Turn off GPIO warnings if you get "already in use" warnings
+GPIO.setwarnings(False)
+
+# Set pin numbering mode
+GPIO.setmode(GPIO.BOARD)
+
 class HCSR04:
-#Encapsulates the attributes and methods to use the HC-SR04 ultra-sound distance sensor
-    trig = 0
-    echo = 0
-    const_cm = 17014.50
-    const_in = 6698.62
-    const_ft = 558.2
+    """
+    Simple HC-SR04 ultrasonic sensor driver.
+    """
     def __init__(self, trig, echo):
         self.trig = trig
         self.echo = echo
 
-        gpio.setmode(gpio.BOARD)
-        gpio.setup(self.trig, gpio.OUT)
-        gpio.setup(self.echo, gpio.IN)
-        gpio.output(self.trig, False)
-        # Sleep for 0.3 s for the sensor to settle
-        time.sleep(0.3)
+        # Setup pins
+        GPIO.setup(self.trig, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
 
-    def __del__(self):
-        gpio.cleanup()
-        print("all clean")
-    
-    #Measures the distance and return the distance in the desired unit
-    def measure(self, samples, unit):
-        count = 0
-        distance = 0.0
-        pulse_start = 0
-        pulse_end = 0
-        acc = 0
-        while count < samples:
+        # Initialize trigger pin to Low
+        GPIO.output(self.trig, False)
+        # Give the sensor a moment to settle
+        time.sleep(0.2)
 
-            gpio.output(self.trig, True)
+    def measure(self, samples=5, unit="cm"):
+        """
+        Perform multiple measurements and return the averaged distance.
+        
+        :param samples: number of samples to average
+        :param unit: 'cm' for centimeters or 'inch' for inches
+        :return: averaged distance in the specified unit or None on timeout
+        """
+        accumulator = 0.0
+
+        for _ in range(samples):
+            # Send 10us pulse to trigger
+            GPIO.output(self.trig, True)
             time.sleep(0.00001)
-            gpio.output(self.trig, False)
-            
+            GPIO.output(self.trig, False)
+
             start = time.time()
-            while gpio.input(self.echo) == 0:
-                if time.time() - start > 0.05:
-                    return None  # Timeout if no echo received
+            pulse_start = 0
+            pulse_end = 0
+
+            # Wait for echo to go HIGH
+            while GPIO.input(self.echo) == 0:
+                if time.time() - start > 0.05:  # 50ms timeout
+                    return None
                 pulse_start = time.time()
-            while gpio.input(self.echo) == 1:
-                if time.time() - start > 0.05:
+
+            # Wait for echo to go LOW
+            while GPIO.input(self.echo) == 1:
+                if time.time() - start > 0.05:  # 50ms timeout
                     return None
                 pulse_end = time.time()
-            
+
+            # Calculate pulse duration
             pulse_duration = pulse_end - pulse_start
-            if(unit == "cm"):
-                distance = pulse_duration * self.const_cm
-            elif(unit == "in"):
-                distance = pulse_duration * self.const_in
-            elif(unit == "ft"):
-                distance = pulse_duration * self.const_ft
+            # Distance in centimeters (speed of sound ~34300 cm/s, round trip factor of 2)
+            distance_cm = pulse_duration * 17150
+
+            if unit.lower() == "inch":
+                # Convert cm -> inches
+                distance = distance_cm / 2.54
             else:
-                distance = 0.0
-            acc += distance
-            count += 1
+                # Default to cm
+                distance = distance_cm
+
+            accumulator += distance
+
+        average_distance = accumulator / samples
+        return round(average_distance, 2)
+
+def sonar_loop(sensor, samples, stop_event):
+    """
+    Continuously measure distance and print results until stop_event is set.
+    """
+    try:
+        while not stop_event.is_set():
+            distance = sensor.measure(samples, "cm")
+            if distance is not None:
+                print("Distance:", distance, "cm")
+                if distance < 10:
+                    print("Object detected! Stopping/rerouting...")
+            else:
+                print("Sensor timeout or error.")
             
-        acc = round(acc / samples, 2)
-        return acc
+            # Adjust the sleep time as needed for your application
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("Sonar thread interrupted by user.")
+    finally:
+        print("Sonar loop finishing.")
 
+def main():
+    # Create sensor instance on pins 7 (TRIG) and 12 (ECHO)
+    sensor = HCSR04(trig=7, echo=12)
 
-from HCSR04 import HCSR04
-samples = 5
-#creation of sonar sensor
-sensor = HCSR04(7, 12)
-#Function for sonar sensor takes HCSR04 object and sample number for accuracy of distance
-def Sonar(sensor, samples):
-    while(True):
-        
-        distance = sensor.measure(samples, "cm")
-    
-        if distance is not None:
-            print ("Distance:", distance, "cm")
-            if distance < 10:
-                print("Object detected, stopping robot and rerouting.....")
-        else:
-            print ("Sensor timeout or error")
-        time.sleep(0.1)
+    # Event to signal the sonar thread to stop
+    stop_event = threading.Event()
 
-sensorThread = threading.Thread(target = Sonar, args = (sensor, samples))
-sensorThread.start()
+    # Create and start the sonar thread
+    samples = 5
+    sensor_thread = threading.Thread(target=sonar_loop, args=(sensor, samples, stop_event))
+    sensor_thread.start()
+
+    try:
+        # Main thread doing other stuff
+        # For demonstration, just wait here until user hits Ctrl+C
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Main program interrupted by user.")
+    finally:
+        # Signal the sonar thread to stop
+        stop_event.set()
+        sensor_thread.join()
+        # Cleanup GPIO once, at the very end
+        GPIO.cleanup()
+        print("GPIO cleanup done. Program exiting.")
+
+if __name__ == "__main__":
+    main()
